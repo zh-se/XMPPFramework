d9dcb531849bf0eba240d4fc696e1cec2c114901
sergey.zhdanov <zhdanov.s.g@gmail.com>
2015-09-01 12:58:10 +0300

    podspec added

---
+++ XMPPFramework.h
@@ -0,0 +1,21 @@
+#import "XMPP.h"
+
+// List the modules you're using here.
+
+#import "XMPPReconnect.h"
+
+#import "XMPPRoster.h"
+#import "XMPPRosterCoreDataStorage.h"
+
+#import "XMPPvCardTempModule.h"
+#import "XMPPvCardAvatarModule.h"
+#import "XMPPvCardCoreDataStorage.h"
+
+#import "XMPPCapabilities.h"
+#import "XMPPCapabilitiesCoreDataStorage.h"
+
+#import "XMPPMUC.h"
+#import "XMPPRoomCoreDataStorage.h"
+
+#import "XMPPMessageArchiving.h"
+#import "XMPPMessageArchivingCoreDataStorage.h"
